78eb47118c63337f50529e8bb497867e81cdae3a
BirddWang <f74111102@gs.ncku.edu.tw>
2024-10-24 16:58:23 +0800

    fix: no use of mulwf

--- lab5/bonus.asm
+++ lab5/bonus.asm
@@ -3,8 +3,8 @@ GLOBAL _multi_signed
 PSECT mytext,local,class=CODE,reloc=2
 
 _multi_signed:
-    movff WREG, 0x050 ; a
-    movff 0x001, 0x051 ; b
+    movff WREG, 0x050 ; a 8bit -128-127
+    movff 0x001, 0x051 ; b 4bit -8, 7
     ; check msb of b
     ; signed flag: 0x060
     clrf 0x060
@@ -26,12 +26,36 @@ neg_b:
     negf 0x051
     
 multiply:
-    movff 0x050, WREG
-    mulwf 0x051
+    rcall mul_main
     btfsc 0x060, 0
 	goto negation
     goto result
 
+mul_main:
+    movlw 0x08
+    movwf 0x61 ; time
+    ; 0x050 * 0x051 = PRODH:PRODL
+mul_loop:
+    btfsc 0x50, 7
+	rcall add_
+    decfsz 0x61
+	goto rotate
+    return
+rotate:
+    bcf STATUS, 0
+    rlcf PRODL
+    rlcf PRODH
+    bcf STATUS, 0
+    rlcf 0x50
+    goto mul_loop
+    
+add_:
+    movff 0x051, WREG
+    addwf PRODL, f
+    btfsc STATUS, 0
+	incf PRODH
+    return
+    
 negation:
     clrf WREG
     cpfseq PRODH
