8e053c933bb61d8316bab75aef93948e5ad67e1e
BirddWang <f74111102@gs.ncku.edu.tw>
2025-01-14 20:10:33 +0800

    update

--- proj/main.c
+++ proj/main.c
@@ -10,6 +10,7 @@
 char str[20];
 long long int constant_pwm = 2000;
 float curr_duty_per = 0.2;
+float curr_duty_per_2 = 0.2;
 void main(void) 
 {    
     SYSTEM_Initialize() ;
@@ -18,66 +19,8 @@ void main(void)
     T3CONbits.TMR3ON = 1;
     while(1);
 }
-
 void __interrupt(high_priority) Hi_ISR(void)
 {
 }
 
 // void interrupt low_priority Lo_ISR(void)
-void __interrupt(low_priority)  Lo_ISR(void)
-{
-    //Uart
-    if(RCIF)
-    {
-        if(RCSTAbits.OERR)
-        {
-            CREN = 0;
-            Nop();
-            CREN = 1;
-        }
-        MyusartRead();
-        
-        RCIF = 0;
-    }
-    
-    // TMR1
-    if(CCP1IF) {
-        // 2000 --> 65535-2000*k
-        T1CONbits.TMR1ON = 0;
-        long long int curr = (CCPR1H << 4) | CCPR1L;
-        if(curr == 65535 - 2000*curr_duty_per) {
-            curr = 65535 - 2000*(1-curr_duty_per);
-        }
-        else {
-            curr = 65535 - 2000*curr_duty_per;
-        }
-        TMR1H = (curr>>4);
-        TMR1L = curr & 0b1111;
-        CCP1IF = 0;
-    }
-    if(CCP2IF) {
-        T3CONbits.TMR3ON = 0;
-        long long int curr = (CCPR2H << 4) | CCPR1L;
-        if(curr == 65535 - 2000*curr_duty_per) {
-            curr = 65535 - 2000*(1-curr_duty_per);
-        }
-        else {
-            curr = 65535 - 2000*curr_duty_per;
-        }
-        TMR3H = (curr>>4);
-        TMR3L = curr & 0b1111;
-        CCP2IF = 0;
-    }
-    if(TMR1IF) {
-        TMR1IF = 0;
-    }
-    // TMR2
-    if(TMR2IF) {
-        TMR2IF = 0;
-    }
-    // TMR3
-    if(TMR3IF) {
-        TMR3IF = 0;
-    }
-    return;
-}
--- proj/setting_hardware/uart.c
+++ proj/setting_hardware/uart.c
@@ -1,9 +1,36 @@
 #include <xc.h>
     //setting TX/RX
-
+long long int constant_pwm = 2000;
+float curr_duty_per = 0.2;
+float curr_duty_per_2 = 0.2;
 char mystring[20];
 int lenStr = 0;
-
+ void XL_Motor_Control(char direction) {
+    // ??????????
+    if (direction == 'W') { // Forward
+        LATAbits.LATA0 = 1;   // IN1 = H
+        LATAbits.LATA1 = 0;   // IN2 = L
+        curr_duty_per = 0.2;         // ??? 100%
+    } else if (direction == 'S') { // Backward
+        LATAbits.LATA0 = 0;   // IN1 = L
+        LATAbits.LATA1 = 1;   // IN2 = H
+        curr_duty_per = 0.2;         // ??? 100%
+    } else if (direction == 'A') {
+        LATAbits.LATA2 = 1;   // IN1 = H
+        LATAbits.LATA3 = 0;   // IN2 = L
+        curr_duty_per = 0.2;         // ??? 100%
+    } else if (direction == 'D') {
+        LATAbits.LATA2 = 0;   // IN1 = L
+        LATAbits.LATA3 = 1;   // IN2 = H
+    } else { // Stop
+        LATAbits.LATA0 = 0;
+        LATAbits.LATA1 = 0;
+        LATAbits.LATA2 = 0;   // IN1 = L
+        LATAbits.LATA3 = 0;   // IN2 = L
+        curr_duty_per = 0;           // ??? 0%
+    }
+}
+ 
 void UART_Initialize() {
            
     /*       TODObasic   
@@ -58,6 +85,7 @@ void MyusartRead()
 {
     /* TODObasic: try to use UART_Write to finish this function */
     char received = RCREG;
+    XL_Motor_Control(received);
     UART_Write(received);
     if(received == '\n' || received == '\r' || lenStr>=19) {
         ClearBuffer();
@@ -71,4 +99,66 @@ void MyusartRead()
 
 char *GetString(){
     return mystring;
+}
+
+void __interrupt(low_priority)  Lo_ISR(void)
+{
+    //Uart
+    if(RCIF)
+    {
+        if(RCSTAbits.OERR)
+        {
+            CREN = 0;
+            Nop();
+            CREN = 1;
+        }
+        MyusartRead();
+        
+        RCIF = 0;
+    }
+    
+    // TMR1
+    if(CCP1IF) {
+        // 2000 --> 65535-2000*k
+        T1CONbits.TMR1ON = 0;
+        long long int curr = (CCPR1H << 4) | CCPR1L;
+        if(curr == 65535 - 2000*curr_duty_per) {
+            curr = 65535 - 2000*(1-curr_duty_per);
+        }
+        else {
+            curr = 65535 - 2000*curr_duty_per;
+        }
+        CCPR1H = (curr>>4);
+        CCPR1L = curr & 0b1111;
+        TMR1H = 0;
+        TMR1L = 0;
+        CCP1IF = 0;
+    }
+    if(CCP2IF) {
+        T3CONbits.TMR3ON = 0;
+        long long int curr = (CCPR2H << 4) | CCPR1L;
+        if(curr == 65535 - 2000*curr_duty_per_2) {
+            curr = 65535 - 2000*(1-curr_duty_per_2);
+        }
+        else {
+            curr = 65535 - 2000*curr_duty_per_2;
+        }
+        CCPR2H = (curr>>4);
+        CCPR2L = curr & 0b1111;
+        TMR3H = 0;
+        TMR3L = 0;
+        CCP2IF = 0;
+    }
+    if(TMR1IF) {
+        TMR1IF = 0;
+    }
+    // TMR2
+    if(TMR2IF) {
+        TMR2IF = 0;
+    }
+    // TMR3
+    if(TMR3IF) {
+        TMR3IF = 0;
+    }
+    return;
 }
